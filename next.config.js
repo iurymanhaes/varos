bb63b6464ee403ea3c5ed5f1ad9485db8cc741b9
Iury Manhães <iury.manhaes@gmail.com>
2024-01-19 13:03:35 -0300

    cors next config

--- next.config.js
+++ next.config.js
@@ -3,15 +3,17 @@ const nextConfig = {
     async headers() {
         return [
           {
-            source: "/api/form",
+            // matching all API routes
+            source: "/api/:path*",
             headers: [
+              { key: "Access-Control-Allow-Credentials", value: "true" },
               { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "POST" },
-              { key: "Access-Control-Allow-Headers", value: "Content-Type" },
-            ],
-          },
-        ];
-      },
+              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+          }
+        ]
+      }
 }
 
 module.exports = nextConfig
